e94a3b6ed02fb130d1d9db38a521f106a0c9d7b7
chanduzumba <chanduzumba@gmail.com>
2026-06-28 13:56:25 +0530

    express server code and dependency update

---
+++ node-backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express' //import express for creating a server
+import dotenv from 'dotenv'
+import morgan from 'morgan' //import morgan for logging HTTP requests
+import cors from 'cors'
+
+//configure dot env for reading env variables
+dotenv.config()
+// create and listen at port 5000 from env file
+const app = express()
+
+//listen at port 5000
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Server is running at port: ${port}`)
+})
+
+//middleware to parse incoming request bodies in JSON format
+app.use(express.json());
+app.use(morgan('dev')); //use morgan for logging HTTP requests
+app.use(cors()) // use cors to allow cross-origin requests from frontend running on different port
+
+//root route to check if server is running
+app.get('/' , (req,res)=>{
+    res.send("HELLO FROM ROOT ROUTE")
+})
